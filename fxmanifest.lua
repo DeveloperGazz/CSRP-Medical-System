cb2d2de0bc945e2a93eebdd54adfb31b4ca1e817
DeveloperGazz <Garywkemp@hotmail.com>
2026-02-10 22:59:13 +0000

    Initial commit: Add CSRP Medical System core files with dark mode support and comprehensive injury system

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,33 +1,38 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'CSRP Development'
-description 'Comprehensive Medical System for FiveM UK RP Server'
+author 'DeveloperGazz'
+description 'CSRP Medical System - Comprehensive Medical RP System'
 version '1.0.0'
+
+lua54 'yes'
 
 -- Client Scripts
 client_scripts {
     'config.lua',
-    'modules/injuries.lua',
-    'modules/treatments.lua',
-    'modules/equipment.lua',
+    'modules/*.lua',
     'client/main.lua',
     'client/injury.lua',
     'client/vitals.lua',
     'client/progression.lua',
     'client/treatments.lua',
-    'client/ui.lua'
+    'client/ui.lua',
+    'client/bystanders.lua',
+    'client/psychological.lua',
+    'client/mci.lua',
+    'client/complications.lua',
+    'client/equipment.lua',
+    'client/patient_experience.lua'
 }
 
 -- Server Scripts
 server_scripts {
     'config.lua',
-    'modules/injuries.lua',
-    'modules/treatments.lua',
-    'modules/equipment.lua',
+    'modules/*.lua',
     'server/main.lua',
     'server/sync.lua',
-    'server/admin.lua'
+    'server/admin.lua',
+    'server/logging.lua'
 }
 
 -- UI Files
@@ -35,8 +40,14 @@ ui_page 'html/index.html'
 
 files {
     'html/index.html',
-    'html/css/style.css',
-    'html/js/app.js',
-    'html/assets/*.png',
-    'html/assets/*.svg'
+    'html/patient.html',
+    'html/paramedic.html',
+    'html/mci.html',
+    'html/css/*.css',
+    'html/js/*.js',
+    'html/assets/**/*'
+}
+
+dependencies {
+    -- Standalone - No dependencies!
 }
