baeffc49ad9dbd84f6d6be45a91951416d315842
DeveloperGazz <Garywkemp@hotmail.com>
2026-02-11 00:26:19 +0000

    Add commands to config.lua for injury and healing functions

--- config.lua
+++ config.lua
@@ -1,40 +1,7 @@
-Config = {}
-
-Config.VitalSigns = {
-    Consciousness = {
-        Alert = 4,
-        Voice = 3,
-        Pain = 2,
-        Unresponsive = 1
-    }
+Config.Commands = {
+    addinjury = {command = 'addinjury', description = 'Adds an injury to the player.'},
+    removeinjury = {command = 'removeinjury', description = 'Removes an injury from the player.'},
+    setparamedic = {command = 'setparamedic', description = 'Sets a player as a paramedic.'},
+    heal = {command = 'heal', description = 'Heals the player.'},
+    setblood = {command = 'setblood', description = 'Sets the blood level of the player.'}
 }
-
-Config.Permissions = {
-    UsePatientMenu = true,
-    UseParamedicMenu = false
-}
-
-Config.Hospitals = {
-    {
-        name = "Pillbox Hospital",
-        coords = vector3(298.5, -584.5, 43.3),
-        resupply = true
-    }
-}
-
-Config.Progression = {
-    ProgressionInterval = 10000,
-    BleedingProgressionRate = 0.1,
-    BleedingRate = 1.0,
-    BloodLossPerTick = 0.5,
-    ShockThreshold = 50,
-    EnableComplications = true,
-    CardiacArrestChance = 0.01,
-    AirwayObstructionChance = 0.05,
-    DiastolicRatio = 0.6
-}
-
-Config.Animations = {}
-Config.Debug = true
-
-return Config
